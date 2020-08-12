db27ed5b2f572848af7494825182995d12f0e80d
yi-dai <851758415@qq.com>
2020-08-12 19:00:28 +0800

    ex04.

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import NotMatch from './NotMatch';
+import { Route } from "react-router";
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
-};
 
+class User extends Component {
+  render() {
+    const userId = this.props.match.params.user;
+    let result = /^\d+$/.test(userId);
+    if(result){
+      return <div>User profile page.</div>
+    } else {
+      return <Route component={NotMatch}></Route>
+    }
+  }
+}
 export default User;
